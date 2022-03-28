c7def2fde908ad580fdea73b69d8e88567f504fb
Ishrak <irik77587@gmail.com>
2022-03-28 17:40:28 +0600

    reloading page to retrieve old data works

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,30 @@
-var api = "http://localhost:3000/blobs";
-var message = document.querySelector('input[type="text"]');
+const api = "http://localhost:3000/blobs",
+  ul = document.querySelector("ul"),
+  message = document.querySelector('input[type="text"]');
+
+window.onload = function(){reload();}
+function reload() {
+  //fetch('http://localhost:3000/blobs').then(x => x.text()).then(y=>ul.replaceChildren(y))
+  fetch('http://localhost:3000/blobs')
+    .then(x => x.json())
+    .then(y => y.map((v,i) =>{
+        c = document.createElement("button");
+        c.innerText = '-';
+        c.onclick = function() {
+          eras(i).then(d=> console.log(d))
+        };
+        p = document.createElement("p");
+        p.innerText = v;
+        li = document.createElement("li");
+        li.appendChild(c);
+        li.appendChild(p);
+        return li;
+      })
+    ).then(z => {
+        ul.replaceChildren();
+        z.map(v => {ul.appendChild(v)})
+    })
+}
 
 function send() {
   if(message.value == '')
@@ -8,6 +33,7 @@ function send() {
     method: 'POST',
     body: message.value
   }).then(response => {
+    reload()
     message.value = '';
     return response.text();
   });
@@ -18,5 +44,6 @@ async function eras(i) {
     method: 'DELETE',
     body: i
   });
+  reload();
   return response.text();
 }
